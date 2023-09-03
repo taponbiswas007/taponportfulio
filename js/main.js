6523cd1893110a1ce081f2faefa13b39c6dca32d
taponbiswas007 <taponbiswassdp@gmail.com>
2023-09-03 18:36:13 +0600

    update progress

--- js/main.js
+++ js/main.js
@@ -33,6 +33,10 @@ const numb1 = document.querySelector(".progress-value");
 const numb2 = document.querySelector(".html-progress-value");
 const numb3 = document.querySelector(".css-progress-value");
 const numb4 = document.querySelector(".cssbar");
+const numb5 = document.querySelector(".js-progress-value");
+const numb6 = document.querySelector(".jsbar");
+const numb7 = document.querySelector(".bootstrap-progress-value");
+const numb8 = document.querySelector(".bootstrapbar");
 let counter = 0;
 
 const interval = setInterval(() => {
@@ -42,7 +46,7 @@ const interval = setInterval(() => {
     counter += 1;
     numb1.textContent = counter + "%";
 
-    if (counter === 95) {
+    if (counter <= 95) {
       numb2.textContent = counter + "%";
     }
     if (counter <= 90) {
@@ -50,6 +54,18 @@ const interval = setInterval(() => {
     }
     if (counter <= 90) {
       numb4.textContent = counter + "%";
+    }
+    if (counter <= 66) {
+      numb5.textContent = counter + "%";
+    }
+    if (counter <= 66) {
+      numb6.textContent = counter + "%";
+    }
+    if (counter <= 90) {
+      numb7.textContent = counter + "%";
+    }
+    if (counter <= 90) {
+      numb8.textContent = counter + "%";
     }
   }
 }, 31);
